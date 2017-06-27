c7135265d15c73e68051fcad59c60dd5fa880101
officegirl <officegirl@ioet.com>
2017-06-26 21:16:49 -0500

    working on userIntegridad

---
+++ integridad-api/src/main/java/com/mrzolution/integridad/app/conf/WebMvcConfiguration.java
@@ -0,0 +1,40 @@
+package com.mrzolution.integridad.app.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
+
+/**
+ * Created by rene on 19/10/16.
+ */
+@Configuration
+public class WebMvcConfiguration extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        HttpMessageConverter desired = null;
+        for (HttpMessageConverter converter : converters) {
+            if (converter instanceof StringHttpMessageConverter) {
+                desired = converter;
+                break;
+            }
+        }
+        converters.remove(desired);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+
+    }
+
+}
