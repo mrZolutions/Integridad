f05fda5e4beed6229db5f6d23110d65e186bc924
officegirl <officegirl@ioet.com>
2017-07-10 23:32:28 -0500

    added loading spin

--- integridad-ui/app/views/main.js
+++ integridad-ui/app/views/main.js
@@ -10,15 +10,19 @@
 angular.module('integridadUiApp')
   .controller('MainCtrl', function ($location, authService) {
     var vm = this;
+    vm.loading = false;
 
     vm.error = undefined;
     vm.success = undefined;
 
     vm.login = function(){
+      vm.loading = true;
       var user = {email: vm.email, password: vm.password};
       authService.authUser(user).then(function (response) {
+        vm.loading = false;
         $location.path('/home');
       }).catch(function (error) {
+        vm.loading = false;
         vm.error = error.data;
       });
     }
@@ -30,11 +34,14 @@ angular.module('integridadUiApp')
       if(vm.email === '' || vm.password === ''){
         vm.error = 'Debe ingresar un Email y Password';
       } else {
+        vm.loading = true;
         var user = {email: vm.email, password: vm.password};
         authService.registerUser(user).then(function (response) {
+          vm.loading = false;
           vm.error = undefined;
           vm.success = 'Resgistro realizado con exito. Se envio un email a la cuenta registrada para activar su cuenta';
         }).catch(function (error) {
+          vm.loading = false;
           vm.error = error.data;
         });
       }
