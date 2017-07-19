30231b448a199256639ceb6444f354be1da85d92
officegirl <officegirl@ioet.com>
2017-07-19 00:03:15 -0500

    working on client and security permissions

---
+++ integridad-api/src/main/java/com/mrzolution/integridad/app/services/UserTypePermissionsServices.java
@@ -0,0 +1,36 @@
+package com.mrzolution.integridad.app.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.mrzolution.integridad.app.domain.UserType;
+import com.mrzolution.integridad.app.domain.UserTypePermissions;
+import com.mrzolution.integridad.app.repositories.UserTypePermissionsRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+public class UserTypePermissionsServices {
+	
+	@Autowired
+	UserTypePermissionsRepository userTypePermissionsRepository;
+	
+	public Iterable<UserTypePermissions> findByUserType(UserType userType){
+		Iterable<UserTypePermissions> userTypePermissions = userTypePermissionsRepository.findByUserType(userType);
+		for (UserTypePermissions userTypePermission : userTypePermissions) {
+			userTypePermission.setFatherListToNull();
+		}
+		return userTypePermissions;
+	}
+	
+	
+	
+	private void populateChildren(UserTypePermissions userTypePermisions) {
+		log.info("UserTypePermissionsServices populateChildren userTypePermisionsId: {}", userTypePermisions.getId());
+		
+		userTypePermisions.setFatherListToNull();
+		log.info("UserTypeServices populateChildren FINISHED userTypeId: {}", userTypePermisions.getId());
+	}
+
+}
