35e9ca4ddb7ecebf67c6be0fb653f1cc5cb9db81
daniel <darcos@ioet.com>
2018-05-02 15:13:57 -0500

    removed console logs

--- integridad-ui/app/scripts/services/date.service.js
+++ integridad-ui/app/scripts/services/date.service.js
@@ -2,10 +2,8 @@ angular
   .module('app.services')
   .service('dateService', function () {
     this.getIsoDate = function(dateBillPre){
-      console.log('---- 1: ', dateBillPre)
       // var dateReturn = new Date(dateBillPre.getTime() - (5*60*60*1000));
       var dateReturn = new Date(dateBillPre.getTime());
-      console.log('---- 2: ', dateReturn)
       return dateReturn.toISOString().toString();
     }
   });
