ea9995cdc9c5b6bdd9198079b1136491547a9f2c
daniel <darcos@ioet.com>
2018-05-02 15:11:31 -0500

    fixed getIsoDate

--- integridad-ui/app/scripts/services/date.service.js
+++ integridad-ui/app/scripts/services/date.service.js
@@ -2,7 +2,10 @@ angular
   .module('app.services')
   .service('dateService', function () {
     this.getIsoDate = function(dateBillPre){
-      var dateReturn = new Date(dateBillPre.getTime() - (5*60*60*1000));
+      console.log('---- 1: ', dateBillPre)
+      // var dateReturn = new Date(dateBillPre.getTime() - (5*60*60*1000));
+      var dateReturn = new Date(dateBillPre.getTime());
+      console.log('---- 2: ', dateReturn)
       return dateReturn.toISOString().toString();
     }
   });
