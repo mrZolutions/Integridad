19b7bf4974c5dad4a72e5740fe2222e68024c5a1
Daniel Arcos <danielgokuarcos@yahoo.com>
2018-06-01 01:12:28 -0500

    worked on retention report

---
+++ integridad-api/src/main/java/com/mrzolution/integridad/app/domain/report/RetentionReport.java
@@ -0,0 +1,34 @@
+package com.mrzolution.integridad.app.domain.report;
+
+import lombok.Data;
+
+@Data
+public class RetentionReport {
+    private String date;
+    private String documentDate;
+    private String providerCode;
+    private String providerName;
+    private String ruc;
+    private String retentionNumber;
+    private String authorizationNumber;
+    private String ejercicioFiscal;
+    private Double total;
+    private String subsidiary;
+    private String userName;
+    private String status;
+
+    public RetentionReport(String date, String documentDate, String providerCode, String providerName, String ruc, String retentionNumber, String authorizationNumber, String ejercicioFiscal, Double total, String subsidiary, String userName, String status) {
+        this.date = date;
+        this.documentDate = documentDate;
+        this.providerCode = providerCode;
+        this.providerName = providerName;
+        this.ruc = ruc;
+        this.retentionNumber = retentionNumber;
+        this.authorizationNumber = authorizationNumber;
+        this.ejercicioFiscal = ejercicioFiscal;
+        this.total = total;
+        this.subsidiary = subsidiary;
+        this.userName = userName;
+        this.status = status;
+    }
+}
