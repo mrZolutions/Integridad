c2abadb7825f33e032cfe28eb9a626669432c1b2
Daniel Arcos <danielgokuarcos@yahoo.com>
2018-06-24 22:34:03 -0500

    added quotation

--- integridad-ui/app/views/bills/bill.ctrl.js
+++ integridad-ui/app/views/bills/bill.ctrl.js
@@ -595,14 +595,13 @@ angular.module('integridadUiApp')
 
       var req = requirementService.createRequirement(vm.clientSelected, vm.bill, $localStorage.user, vm.impuestosTotales, vm.items, vm.pagos);
 
-      console.log(JSON.stringify(req, null, 4));
       billService.getClaveDeAcceso(req, vm.companyData.userClient.id).then(function(resp){
         vm.bill.pagos = vm.pagos;
         if(vm.bill.discountPercentage === undefined){
           vm.bill.discountPercentage = 0;
         }
-        // var obj = JSON.parse(resp.data);
-        var obj = {clave_acceso: '1234560', id:'id12345'};
+        var obj = JSON.parse(resp.data);
+        // var obj = {clave_acceso: '1234560', id:'id12345'};
         if(obj.errors === undefined){
           vm.bill.claveDeAcceso = obj.clave_acceso;
           vm.bill.idSri = obj.id;
