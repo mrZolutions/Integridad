68bea43ebe7b36a10e60d0a13c31ee76a146a501
DanielFRB <danfelrondon@gmail.com>
2018-08-31 10:19:44 -0500

    Cambios en modulos JAVA y en FrontEnd

--- integridad-api/src/main/java/com/mrzolution/integridad/app/domain/report/RetentionReport.java
+++ integridad-api/src/main/java/com/mrzolution/integridad/app/domain/report/RetentionReport.java
@@ -11,13 +11,14 @@ public class RetentionReport {
     private String ruc;
     private String retentionNumber;
     private String authorizationNumber;
+    private String documentNumber;
     private String ejercicioFiscal;
     private Double total;
     private String subsidiary;
     private String userName;
     private String status;
 
-    public RetentionReport(String date, String documentDate, String providerCode, String providerName, String ruc, String retentionNumber, String authorizationNumber, String ejercicioFiscal, Double total, String subsidiary, String userName, String status) {
+    public RetentionReport(String date, String documentDate, String providerCode, String providerName, String ruc, String retentionNumber, String authorizationNumber, String documentNumber, String ejercicioFiscal, Double total, String subsidiary, String userName, String status) {
         this.date = date;
         this.documentDate = documentDate;
         this.providerCode = providerCode;
@@ -25,6 +26,7 @@ public class RetentionReport {
         this.ruc = ruc;
         this.retentionNumber = retentionNumber;
         this.authorizationNumber = authorizationNumber;
+        this.documentNumber = documentNumber;
         this.ejercicioFiscal = ejercicioFiscal;
         this.total = total;
         this.subsidiary = subsidiary;
