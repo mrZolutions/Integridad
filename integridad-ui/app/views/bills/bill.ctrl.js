c455ee5bd9fca9c26ebe0ce9ef65743e6be4b71d
Daniel Rondon <danfelrondon@gmail.com>
2018-09-11 17:51:26 -0500

    Cambios al Ticket en el módulo bill.tpl.html

--- integridad-ui/app/views/bills/bill.ctrl.js
+++ integridad-ui/app/views/bills/bill.ctrl.js
@@ -505,7 +505,7 @@ angular.module('integridadUiApp')
       var innerContents = document.getElementById(printSectionId).innerHTML;
         var popupWinindow = window.open('', '_blank', 'width=600,height=700,scrollbars=no,menubar=no,toolbar=no,location=no,status=no,titlebar=no');
         popupWinindow.document.open();
-        popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="https://github.com/mrZolutions/Integridad/blob/master/integridad-ui/app/styles/style-print.css" /></head><body onload="window.print()">' + innerContents + '</html>');
+        popupWinindow.document.write('<html><head><link rel="stylesheet" type="text/css" href="../../style-print.css" /></head><body onload="window.print()">' + innerContents + '</html>');
         popupWinindow.document.close();
     };
 
