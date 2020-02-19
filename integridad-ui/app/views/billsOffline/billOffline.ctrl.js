6eeaca476ca0dfcc111d06816f2c97aa9c64993c
Daniel Arcos <danielgokuarcos@yahoo.com>
2020-02-19 12:21:29 -0500

    10022020-813 changed f key event to the whole page on the bills

--- integridad-ui/app/views/billsOffline/billOffline.ctrl.js
+++ integridad-ui/app/views/billsOffline/billOffline.ctrl.js
@@ -491,6 +491,17 @@ angular.module('integridadUiApp')
               vm.quantity = undefined;
         };
 
+        vm.openPayMode = function(event){
+            if (event.keyCode === 102 || event.charCode === 102 || event.keyCode === 70 || event.charCode === 70) {
+                $('#modalAddPago').modal('show');
+                vm.medio.medio = vm.medList[0];
+                vm.loadMedio();
+                setTimeout(function() {
+                    document.getElementById("addPaymentBtn").focus();
+                }, 500);
+            };
+        };
+
         vm.fill = function(event) {
             if (event.keyCode === 32 || event.charCode === 32) {
                 if (vm.billOfflineBarCode.length < 13) {
@@ -501,14 +512,6 @@ angular.module('integridadUiApp')
                     vm.billOfflineBarCode = vm.billOfflineBarCodeFixed.trim();
                     vm.filterBarCode();
                 };
-            };
-            if (event.keyCode === 102 || event.charCode === 102 || event.keyCode === 70 || event.charCode === 70) {
-                $('#modalAddPago').modal('show');
-                vm.medio = vm.medList[0];
-                vm.loadMedio();
-                setTimeout(function() {
-                    document.getElementById("addPaymentBtn").focus();
-                }, 500);
             };
         };
 
