98e510d3c9ab89460c8537382578c980d977255e
Daniel Arcos <danielgokuarcos@yahoo.com>
2020-03-09 16:15:55 -0500

    09032020-1613 fixed print format offline

--- integridad-ui/app/views/billsOffline/billOffline.ctrl.js
+++ integridad-ui/app/views/billsOffline/billOffline.ctrl.js
@@ -856,6 +856,13 @@ angular.module('integridadUiApp')
                         vm.billOfflineCreated = respBill;
                         $localStorage.user.cashier.billOfflineNumberSeq = vm.billOffline.billSeq;
                         vm.loading = false;
+                        vm.rowsToFill = [];
+                        for (var i = vm.billOffline.detailsOffline.length; i < 9; i++) {
+                            console.log('-*-*-*-*-*-*-*-*-*-*-*-* : ', i)
+                            vm.rowsToFill.push(i)
+                        }
+
+                        console.log('-*-*-*-*-*-*-*-*-*-*-*-* PRE: ', vm.rowsToFill);
                         setTimeout(function() {
                             // vm.user.cashier.specialPrint ? vm.printToCart('printMatrixBillId') : document.getElementById("printBtnBill").click();
                             // document.getElementById("printBtnBill").click();
