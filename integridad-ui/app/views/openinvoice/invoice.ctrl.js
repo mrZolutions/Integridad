238610a3172a380111e77e7ebd6e250575b92597
Daniel Arcos <danielgokuarcos@yahoo.com>
2020-07-06 19:11:44 -0500

    11062020-1720 Fixed open invoice format

--- integridad-ui/app/views/openinvoice/invoice.ctrl.js
+++ integridad-ui/app/views/openinvoice/invoice.ctrl.js
@@ -9,12 +9,25 @@
 angular.module('integridadUiApp')
     .controller('InvoiceCtrl', function(_, $routeParams, $http) {
 
+        var payForm = {
+            "efectivo": {name:'20 - OTROS CON UTILIZACION DEL SISTEMA FINANCIERO', value: '20'},
+            "dinero_electronico_ec": {name:'20 - OTROS CON UTILIZACION DEL SISTEMA FINANCIERO', value: '20'},
+            "transferencia": {name:'20 - OTROS CON UTILIZACION DEL SISTEMA FINANCIERO', value: '20'},
+            "credito": {name:'20 - OTROS CON UTILIZACION DEL SISTEMA FINANCIERO', value: '20'},
+            "cheque": {name:'20 - OTROS CON UTILIZACION DEL SISTEMA FINANCIERO', value: '20'},
+            "cheque_posfechado": {name:'20 - OTROS CON UTILIZACION DEL SISTEMA FINANCIERO', value: '20'},
+            "tarjeta_credito": {name:'19 - TARJETA DE CREDITO', value: '19'},
+            "tarjeta_debito": {name:'19 - TARJETA DE CREDITO', value: '19'},
+        }
+
         var vm = this;
         vm.error = undefined;
         vm.success = undefined;
         vm.data = undefined;
         vm.invoiceNumber = undefined;
         vm.mailClient = undefined;
+        vm.pagos = undefined;
+        vm.cambio = undefined;
 
         vm.baseUrl = 'https://invoicesmrz.herokuapp.com/invoices/print/';
         vm.config = {};
@@ -23,7 +36,7 @@ angular.module('integridadUiApp')
         };
         vm.options = {
             width: 2,
-            height: 100,
+            height: 60,
             quite: 10,
             displayValue: true,
             font: "monospace",
@@ -42,13 +55,27 @@ angular.module('integridadUiApp')
             vm.mailClient = data.comprador.email.split(',')[0];
 
             vm.txt = vm.data.clave_acceso;
+
+            console.log('credito', vm.data.credito)
+            console.log('pagos', vm.data.pagos)
+            if(vm.data.credito){
+                vm.pagos = {name: "CREDITO", code: payForm["credito"].name, total: vm.data.credito.monto}
+            } else if(vm.data.pagos){
+                vm.pagos = {
+                    name: vm.data.pagos[0].medio, 
+                    code: payForm[vm.data.pagos[0].medio].name,
+                    total: vm.data.pagos[0].total
+                }
+            }
+
+            vm.cambio = (parseFloat(vm.data.totales.importe_total) - parseFloat(vm.pagos.total)).toFixed(2)
         }
 
 
         vm.clickPdf = function (){
             var element = document.getElementById('printArea');
             var opt = {
-                margin:       0.5,
+                margin:       0.4,
                 filename:     'factura.pdf',
                 image:        { type: 'jpeg', quality: 0.98 },
                 html2canvas:  { dpi: 192, letterRendering: true },
