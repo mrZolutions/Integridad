f3a87915e43eb4f5d33d24702fda126de900bee9
Daniel Arcos <danielgokuarcos@yahoo.com>
2020-07-22 23:41:33 -0500

    22072020-1030 Added resend no autorizado option with edition

--- integridad-ui/app/views/bills/nat.ctrl.js
+++ integridad-ui/app/views/bills/nat.ctrl.js
@@ -12,12 +12,33 @@ angular.module('integridadUiApp')
         var vm = this;
         vm.error = undefined;
         vm.success = undefined;
-        vm.baseUrl = 'https://invoicesmrz.herokuapp.com/invoices/';
+        vm.baseUrl = 'https://invoicesmrz.herokuappk.com/invoices/';
         vm.config = {};
         vm.config.headers = {
             'Content-Type':'application/json',
         };
+        vm.tipoImpuesto = {
+            '2':'IVA',
+            '3':'ICE',
+            '5':'IRBPNR'
+        };
+        vm.codigoPorcentaje = {
+            '0':0,
+            '2':12,
+            '3':14
+        };
+        vm.payForms = [
+            { name:'Efectivo', codigo: "efectivo" },
+            { name:'Dinero Electr&oacutenico', codigo: "dinero_electronico_ec" },
+            { name:'Transferencia', codigo: "transferencia" },
+            { name:'Cr&eacute;dito', codigo: "credito" },
+            { name:'Cheque', codigo: "cheque" },
+            { name:'Cheque Posfechado', codigo: "cheque_posfechado" },
+            { name:'Tarjeta de Cr&eacute;dito', codigo: "tarjeta_credito" },
+            { name:'Tarjeta de D&eacute;bito', codigo: "tarjeta_debito" },
+        ];
         vm.billList = [];
+        vm.doc = undefined;
 
         vm.resendBill = function(bill){
             vm.loading = true;
@@ -43,11 +64,73 @@ angular.module('integridadUiApp')
             });
         }
 
+        vm.getTotalDetail = function(index){
+            vm.doc.items[index].precio_total_sin_impuestos = 
+            (vm.doc.items[index].cantidad * vm.doc.items[index].precio_unitario) - 
+            (vm.doc.items[index].descuento);
+
+            _.each(vm.doc.items[index].impuestos, function(imp){ 
+                imp.base_imponible = vm.doc.items[index].precio_total_sin_impuestos;
+                imp.valor = imp.base_imponible * parseFloat('0.'+imp.tarifa);
+            });
+
+            _getTotals()
+        }
+
+        vm.removeDetail = function(index){
+            vm.doc.items.splice(index,1);
+            _getTotals()
+        }
+
+        vm.getTotales = function(){
+            _getTotals();
+        }
+
+        vm.saveResend = function(){
+            vm.loading = true;
+            $http.post(vm.baseUrl + 'reaut/', vm.doc, vm.config).then(function (response) {
+                vm.success = 'Comprobante reenviado';
+                vm.loading = false;
+                vm.doc = undefined;
+                _getData();
+            }).catch(function(error) {
+                console.log(error)
+                vm.loading = false;
+                vm.error = error.data;
+            });
+        }
+
+        function _getTotals() {
+            vm.doc.totales.total_sin_impuestos = 0;
+            var impuestos = 0;
+            _.each(vm.doc.items, function(itm){
+                _.each(itm.impuestos, function(imp){
+                    var impuesto = _.find(vm.doc.totales.impuestos, 
+                        function(impTotales){ 
+                            return  impTotales.codigo === imp.codigo && impTotales.codigo_porcentaje === imp.codigo_porcentaje; 
+                        });
+                    
+                    impuestos += imp.valor;
+                    if(impuesto){
+                        impuesto.base_imponible += imp.base_imponible
+                    } else {
+                        vm.doc.totales.impuestos.push(angular.copy(imp));
+                    }
+                    
+                });
+                vm.doc.totales.total_sin_impuestos += itm.precio_total_sin_impuestos;
+            });
+            vm.doc.totales.importe_total = 
+                vm.doc.totales.total_sin_impuestos + impuestos - vm.doc.totales.descuento + 
+                vm.doc.totales.propina;
+        }
+
         function _getData(){
             vm.loading = true;
             vm.user = holderService.get();
             if(vm.user.token !== null){
-                vm.config.headers['Authorization'] = 'Bearer ' + vm.user.token;
+                // vm.config.headers['Authorization'] = 'Bearer ' + vm.user.token;
+                vm.config.headers['Authorization'] = 'Bearer ' + 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZWUxNWJiNTI5NmI3MDYwZDYwMGRkNjEiLCJlbWFpbCI6ImRhbmllbEBnbWFpbC5jb20iLCJwZXJtaXNzaW9uTGV2ZWwiOjIxNDc0ODM2NDcsInByb3ZpZGVyIjoiZW1haWwiLCJuYW1lIjoiRGFuaWVsIEEuIEFyY29zIiwiY29tcGFueUlkIjoiNWVmMzkwODE1ZDZmOGQzY2RkMDVmMzFmIiwicmVmcmVzaEtleSI6Ikd0NEU3enFqa0MwR2dRVzhrRk1Lc0E9PSIsImlhdCI6MTU5NTQzNjEzNH0.0o2TJmSutBo5HfUzo0Qa5nSbohHd1ez35m9LTH7sM84';
 
                 $http.get(vm.baseUrl + 'nat/company', vm.config).then(function (response) {
                     if(response.data){
